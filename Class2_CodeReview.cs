a88b24366d0786a9613c7c4b1620ce7bfd46fef4
Leonid Tsarev <leo@bastilia.ru>
2016-09-10 11:12:08 +0300

    Create Class2_CodeReview.cs

---
+++ Class2_CodeReview.cs
@@ -0,0 +1,37 @@
+class Class2
+    {
+        private readonly object _sync = new object();
+        private static System.Collections.Hashtable _ht;
+
+        public int GetValue(int index)
+        {
+            Init();
+            return (int)_ht["name_" + index];
+        }
+
+
+        public void Init()
+        {
+            if (_ht == null)
+                lock (_sync)
+                    if (_ht == null)
+                        Create();
+        }
+
+
+        public static void Create()
+        {
+            DataProvider provider = new DataProvider();
+            for (int i = 0; i < 100; i++)
+                _ht["name_" + i] = provider.LongRunningCalculation(i);
+        }
+
+    }
+
+class DataProvider
+{
+    public int LongRunningCalculation(int value)
+       {
+           ....
+       }
+}

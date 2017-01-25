86f98aa8659ab4e7f5ccdb9c138ed3dd765370e6
Leonid Tsarev <leo@bastilia.ru>
2017-01-25 17:04:58 +0300

    Create FooBar.cs

---
+++ FooBar.cs
@@ -0,0 +1,17 @@
+//Input 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+//Output
+//Если делится на 3, то foo
+//Если делится на 5, то bar
+//Если делится на 15, то foobar
+//В противном случае само число
+IEnumerable<string> ConvertToFooBar(IEnumerable<int> enumerable)
+{
+
+
+
+
+
+
+
+
+}

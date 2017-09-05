05260de9b16c98c146a1acefb8a03a5e30f8806f
Leonid Tsarev <leo@bastilia.ru>
2017-09-05 15:26:20 +0300

    Update Class2_CodeReview.cs

--- Class2_CodeReview.cs
+++ Class2_CodeReview.cs
@@ -1,40 +1,41 @@
-CODE FOR REVIEW:
+// CODE FOR REVIEW:
 class Class2
     {
-        private readonly object _sync = new object();
-        private static System.Collections.Hashtable _ht;
+    private readonly object _sync = new object();
+    private static Dictionary<int[], object> _ht;
 
-        public int GetValue(int index)
-        {
-            Init();
-            return (int)_ht["name_" + index];
-        }
-
-
-        public void Init()
-        {
-            if (_ht == null)
-                lock (_sync)
-                    if (_ht == null)
-                        Create();
-        }
-
-
-        public static void Create()
-        {
-            DataProvider provider = new DataProvider();
-            for (int i = 0; i < 100; i++)
-                _ht["name_" + i] = provider.LongRunningCalculation(i);
-        }
-
+    public int GetValue(int index, int index2)
+    {
+        Init();
+        var key = new[] {index, index2};
+        if (_ht.ContainsKey(key) & _ht[key].GetType() == typeof(int))
+            return ((int)_ht[key]);
+        else
+            return null;
     }
 
-EXTERNAL CODE
+    public void Init()
+    {
+        if (_ht == null)
+            lock (_sync)
+                Create();
+    }
 
+    public static void Create()
+    {
+        DataProvider provider = new DataProvider();
+        for (int i = 0; i < 100; i++)
+            for (int j = 1; j <= 12; i++)
+                _ht[new [] { i, j }] = provider.LongRunningCalculation(i, j);
+    }
+
+}
+
+//EXTERNAL CODE
+//Приведен только для справки, никак нельзя менять, код обфусцирован, исходников нет
 public class DataProvider : IDisposable
 {
-    public int LongRunningCalculation(int value)
-       {
-           ....
-       }
+    public int LongRunningCalculation(int value, int value2)
+    {
+    }
 }
